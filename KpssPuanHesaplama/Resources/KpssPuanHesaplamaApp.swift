a8d70cf9acc3e6b940ecbb932985ec83daa56daf
TahirAtakanCan <atkan4202@gmail.com>
2024-07-10 15:20:33 +0300

    feat: File Design

--- KpssPuanHesaplama/Resources/KpssPuanHesaplamaApp.swift
+++ KpssPuanHesaplama/Resources/KpssPuanHesaplamaApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct KpssPuanHesaplamaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView()
         }
     }
 }
---
+++ KpssPuanHesaplama/Views/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  KpssPuanHesaplama
+//
+//  Created by Tahir Atakan Can on 10.07.2024.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
