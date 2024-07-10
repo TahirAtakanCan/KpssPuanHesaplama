d5d2128cc8803c7438f27f335cc54107424e52a3
TahirAtakanCan <atkan4202@gmail.com>
2024-07-10 15:00:36 +0300

    first commit

---
+++ KpssPuanHesaplama/KpssPuanHesaplamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KpssPuanHesaplamaApp.swift
+//  KpssPuanHesaplama
+//
+//  Created by Tahir Atakan Can on 10.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct KpssPuanHesaplamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
