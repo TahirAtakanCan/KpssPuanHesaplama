b4eccaa632dda133f67b2e75540b7c9a6198a261
TahirAtakanCan <atkan4202@gmail.com>
2024-07-11 10:50:51 +0300

    feat: Sonuc label and hesapla button

--- KpssPuanHesaplama/Views/OrtaogretimView.swift
+++ KpssPuanHesaplama/Views/OrtaogretimView.swift
@@ -15,6 +15,7 @@ struct OrtaogretimView: View {
     @State private var gyDogruSayisi = 30
     @State private var gyYanlisSayisi = 0
     
+    @State private var sonuc = 0
     
     var body: some View {
         VStack {
@@ -22,10 +23,43 @@ struct OrtaogretimView: View {
             Form {
                 Section {
                     Stepper("Doğru Sayısı: \(gkDogruSayisi)", value: $gkDogruSayisi, in: 1...60)
+                        .sensoryFeedback(.selection, trigger: gkDogruSayisi)
                     Stepper("Yanlış Sayısı: \(gkYanlisSayisi)", value: $gkYanlisSayisi, in: 1...60)
+                        .sensoryFeedback(.selection, trigger: gkYanlisSayisi)
                     
                 } header: {
                     Text("Genel Kültür")
+                        .textCase(.none)
+                }
+                
+                Section {
+                    Stepper("Doğru Sayısı: \(gkDogruSayisi)", value: $gyDogruSayisi, in: 1...60)
+                        .sensoryFeedback(.selection, trigger: gyDogruSayisi)
+                    Stepper("Yanlış Sayısı: \(gkYanlisSayisi)", value: $gyYanlisSayisi, in: 1...60)
+                        .sensoryFeedback(.selection, trigger: gyYanlisSayisi)
+                    
+                } header: {
+                    Text("Genel Yetenek")
+                        .textCase(.none)
+                }
+                
+                Section {
+                    Text("KPSS Puanı: \(sonuc)")
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Hesapla")
+                            .bold()
+                            .font(.title2)
+                            .padding(8)
+                            .frame(maxWidth: .infinity)
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .tint(.main)
+                    
+                } header: {
+                    Text("Sonuç")
                         .textCase(.none)
                 }
                 
