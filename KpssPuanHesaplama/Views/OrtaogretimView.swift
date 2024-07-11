7b5d40febdbfe124967aec79e3245a7668beff4d
TahirAtakanCan <atkan4202@gmail.com>
2024-07-11 10:38:44 +0300

    feat: Form state

--- KpssPuanHesaplama/Views/OrtaogretimView.swift
+++ KpssPuanHesaplama/Views/OrtaogretimView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct OrtaogretimView: View {
+    
+    @State private var gkDogruSayisi = 30
+    @State private var gkYanlisSayisi = 0
+    
+    @State private var gyDogruSayisi = 30
+    @State private var gyYanlisSayisi = 0
+    
+    
     var body: some View {
         VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            Form {
+                Section {
+                    Stepper("Doğru Sayısı: \(gkDogruSayisi)", value: $gkDogruSayisi, in: 1...60)
+                    Stepper("Yanlış Sayısı: \(gkYanlisSayisi)", value: $gkYanlisSayisi, in: 1...60)
+                    
+                } header: {
+                    Text("Genel Kültür")
+                        .textCase(.none)
+                }
+                
+                
+            }
+            
         }
         .navigationTitle("Ortaöğretim")
     }
