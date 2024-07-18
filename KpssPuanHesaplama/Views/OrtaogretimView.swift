422e22d5714ae5d1dbd61fbe2fe2a69a1a4cf549
TahirAtakanCan <atkan4202@gmail.com>
2024-07-18 12:59:35 +0300

    feat: Dinamik doğru yanlış

--- KpssPuanHesaplama/Views/OrtaogretimView.swift
+++ KpssPuanHesaplama/Views/OrtaogretimView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct OrtaogretimView: View {
     
-    @State private var gkDogruSayisi = 30
-    @State private var gkYanlisSayisi = 0
+    @State private var gyDogruSayisi:Double = 30
+    @State private var gyYanlisSayisi:Double = 0
     
-    @State private var gyDogruSayisi = 30
-    @State private var gyYanlisSayisi = 0
+    @State private var gkDogruSayisi:Double = 30
+    @State private var gkYanlisSayisi:Double = 0
     
     @State private var sonuc = 0
     
@@ -21,26 +21,38 @@ struct OrtaogretimView: View {
         VStack {
             
             Form {
-                Section {
-                    Stepper("Doğru Sayısı: \(gkDogruSayisi)", value: $gkDogruSayisi, in: 1...60)
-                        .sensoryFeedback(.selection, trigger: gkDogruSayisi)
-                    Stepper("Yanlış Sayısı: \(gkYanlisSayisi)", value: $gkYanlisSayisi, in: 1...60)
-                        .sensoryFeedback(.selection, trigger: gkYanlisSayisi)
-                    
-                } header: {
-                    Text("Genel Kültür")
-                        .textCase(.none)
-                }
                 
                 Section {
-                    Stepper("Doğru Sayısı: \(gkDogruSayisi)", value: $gyDogruSayisi, in: 1...60)
+                    Stepper("Doğru Sayısı: \(gyDogruSayisi, specifier: "%.0f")", value: $gyDogruSayisi, in: 0...(60 - gyYanlisSayisi))
                         .sensoryFeedback(.selection, trigger: gyDogruSayisi)
-                    Stepper("Yanlış Sayısı: \(gkYanlisSayisi)", value: $gyYanlisSayisi, in: 1...60)
+                    Stepper("Yanlış Sayısı: \(gyYanlisSayisi, specifier: "%.0f")", value: $gyYanlisSayisi, in: 0...(60 - gyDogruSayisi))
                         .sensoryFeedback(.selection, trigger: gyYanlisSayisi)
                     
                 } header: {
                     Text("Genel Yetenek")
                         .textCase(.none)
+                } footer: {
+                    if(gyDogruSayisi + gyYanlisSayisi > 60) {
+                        Text("Toplam doğru ve yanlış sayıları 60'ı geçemez.")
+                            .foregroundStyle(.red)
+                    }
+                }
+                
+                
+                Section {
+                    Stepper("Doğru Sayısı: \(gkDogruSayisi, specifier: "%.0f")", value: $gkDogruSayisi, in: 0...(60 - gkYanlisSayisi))
+                        .sensoryFeedback(.selection, trigger: gkDogruSayisi)
+                    Stepper("Yanlış Sayısı: \(gkYanlisSayisi, specifier: "%.0f")", value: $gkYanlisSayisi, in: 0...(60 - gkDogruSayisi))
+                        .sensoryFeedback(.selection, trigger: gkYanlisSayisi)
+                    
+                } header: {
+                    Text("Genel Kültür")
+                        .textCase(.none)
+                } footer: {
+                    if(gkDogruSayisi + gkYanlisSayisi > 60) {
+                        Text("Toplam doğru ve yanlış sayıları 60'ı geçemez.")
+                            .foregroundStyle(.red)
+                    }
                 }
                 
                 Section {
@@ -49,7 +61,7 @@ struct OrtaogretimView: View {
                     HesaplaButton(title: "Hesapla") {
                         print("Hesaplama İşlemleri")
                     }
-                    
+                    //.disabled(formKontrol)
                     
                     } header: {
                     Text("Sonuç")
@@ -61,6 +73,13 @@ struct OrtaogretimView: View {
             
         }
         .navigationTitle("Ortaöğretim")
+    }
+    
+    var formKontrol: Bool {
+        if((gyDogruSayisi + gyYanlisSayisi) > 60 || (gkDogruSayisi + gkYanlisSayisi) > 60) {
+            return true
+        }
+        return false
     }
 }
 
