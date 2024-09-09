c443d55a18f00862eaea9841c14fa44252c69b44
TahirAtakanCan <atkan4202@gmail.com>
2024-09-09 17:36:28 +0300

    feat:  Sonuc View and design

---
+++ KpssPuanHesaplama/Views/SharedViews/SonucView.swift
@@ -0,0 +1,18 @@
+//
+//  SonucView.swift
+//  KpssPuanHesaplama
+//
+//  Created by Tahir Atakan Can on 9.09.2024.
+//
+
+import SwiftUI
+
+struct SonucView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SonucView()
+}
