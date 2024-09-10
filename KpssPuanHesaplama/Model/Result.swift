232a84a379beb8634789054756fdb8a9851334f7
TahirAtakanCan <atkan4202@gmail.com>
2024-09-10 18:41:13 +0300

    feat:  Result file code

--- KpssPuanHesaplama/Model/Result.swift
+++ KpssPuanHesaplama/Model/Result.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class Result: Identifiable {
+    
+    @Attribute(.unique) let id = UUID()
+    let sınavAdi: String
+    let gyNet: Double
+    let gkNet: Double
+    let ebNet: Double?
+    let oabtNet: Double?
+    let tarih = Date()
+    let sonuc: Double
+
+    init(sınavAdi: String, gyNet: Double, gkNet: Double, ebNet: Double?, oabtNet: Double?, tarih: Date, sonuc: Double) {
+        self.sınavAdi = sınavAdi
+        self.gyNet = gyNet
+        self.gkNet = gkNet
+        self.ebNet = ebNet
+        self.oabtNet = oabtNet
+        self.tarih = tarih
+        self.sonuc = sonuc
+    }
+    
+}
