f4ea1e567261381f1eb518f30795db48323be59c
TahirAtakanCan <atkan4202@gmail.com>
2024-10-05 13:05:58 +0300

    feat: TargetView v3 (Pıcker design)

--- KpssPuanHesaplama/Views/TargetView.swift
+++ KpssPuanHesaplama/Views/TargetView.swift
@@ -11,8 +11,21 @@ struct TargetView: View {
     @State private var targetScore: Double = 70.0
     @State private var targetDate = Date()
     
+    @State private var selectedBolum = "Ortaöğretim"
+    
+    let bolumler = ["Ortaöğretim", "On Lisans", "Lisans", "Egitim Bilimleri", "OABT"]
+    
     var body: some View {
         Form {
+            Section(header: Text("Bölüm Seçiniz")){
+                Picker("Bölüm Seçiniz", selection: $selectedBolum){
+                    ForEach(bolumler, id: \.self){
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.menu)
+                Text("Seçilen Bölüm: \(selectedBolum)")
+            }
             Section(header: Text("Hedef Puan")) {
                 Slider(value: $targetScore, in: 60...100, step: 0.1)
                 Text("Hedef Puanı: \(targetScore, specifier: "%.1f")")
