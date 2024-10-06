92676dcd9e0d131422d70750e536b0d86c2f62ac
TahirAtakanCan <atkan4202@gmail.com>
2024-10-06 10:29:12 +0300

    feat: Save Button Design

--- KpssPuanHesaplama/Views/SharedViews/KaydetButton.swift
+++ KpssPuanHesaplama/Views/SharedViews/KaydetButton.swift
@@ -5,4 +5,29 @@
 //  Created by Tahir Atakan Can on 6.10.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct KaydetButton: View {
+    
+    let title: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: {
+            action()
+        }, label:{
+            Label(title, systemImage: "square.and.arrow.down.fill")
+                .font(.title3)
+                .foregroundColor(.white)
+                .frame(minWidth: 30, idealWidth: 30, maxWidth: .nan, idealHeight: 30)
+        })
+        .buttonStyle(.borderedProminent)
+        .tint(.main)
+    }
+}
+
+#Preview {
+    KaydetButton(title: "Kaydet", action: {
+        print("Ilk SwiftUI uygulamam")
+    })
+}
